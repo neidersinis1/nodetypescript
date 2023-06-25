d263a9bacb3d93bc8348f464f06779ce455258bd
Neider Sinisterra <neidersinis@hotmail.com>
2023-06-25 12:47:45 -0500

    [Router] dinamic

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-
+import { router } from './routes'
 const app = express()
 
 app.use(cors())
 
 const PORT = process.env.PORT || 3002
+
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`El servidor esta corriendo por el puerto http://localhot:${PORT}`)
